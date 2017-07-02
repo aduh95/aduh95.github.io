3213515c66363251218c7951cf22afe0c9fe6206
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-07-02 18:45:51 +0200

    Experience — Adding technologies and localization links

---
+++ config/experience.default.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace aduh95\Resume\CONFIG\EXPERIENCE;
+
+const ICONS = array(
+    'date' => 'calendar',
+    'place' => 'map-marker',
+    'website' => 'globe',
+);
--- src/views/experience.php
+++ src/views/experience.php
@@ -8,6 +8,8 @@
 
 namespace aduh95\Resume;
 
+use const aduh95\Resume\CONFIG\EXPERIENCE\ICONS;
+
 require_once '../vendor/autoload.php';
 
 
@@ -22,13 +24,59 @@ return function ($doc, $section) {
 
         $list = $article->ul();
 
+        if (empty($infos['info'])) $infos['info'] = array();
+
         foreach ($infos['info'] as $key => $value) {
-            $list[] = $doc->createElement('li')->attr('class', $key)->text($value);
+            $li = $list[] = $doc->createElement('li')
+                ->attr('class', 'fa fa-'.ICONS[$key]);
+
+            switch ($key) {
+                case 'date':
+                    $begin = date_create($value['begin']);
+                    $end = date_create($value['end']);
+                    if ($begin->diff($end)->m) {
+                        $li->span(['lang'=>'en'])->append()
+                            ()->text('From ')->time(['datetime'=>$value['begin']])
+                                ->text($begin->format('F Y'))
+                            ()->text(' to ')->time(['datetime'=>$value['end']])
+                                ->text($end->format('F Y'));
+                        $li->span(['lang'=>'fr'])->append()
+                            ()->text('De ')->time(['datetime'=>$value['begin']])
+                                ->text($begin->format('m/Y'))
+                            ()->text(' à ')->time(['datetime'=>$value['end']])
+                                ->text($end->format('m/Y'));
+                    } else {
+                        $li->span(['lang'=>'en'])
+                            ->time(['datetime'=>$begin->format('Y-m')])
+                                ->text($begin->format('F Y'));
+                        $li->span(['lang'=>'fr'])
+                            ->time(['datetime'=>$begin->format('Y-m')])
+                                ->text($end->format('m/Y'));
+                    }
+                    break;
+
+                case 'website':
+                    $value = [
+                        'link' => $value,
+                        'text' => preg_replace('#^https?://(www\.)?#', '', $value)
+                    ];
+                case 'place':
+                    $li->a()->attr('href', $value['link'])->text($value['text']);
+                    break;
+            }
         }
 
 
         foreach ($infos['description'] as $lang => $text) {
             $article->p(['lang' => $lang, 'class' => 'mission'])->text($text);
+        }
+
+        if (!empty($infos['technologies'])) {
+            $list = $article->ul();
+
+            foreach ($infos['technologies'] as $techno) {
+                $list[] = $doc->createElement('li')->attr('class', 'fa fa-'.$techno['icon'])->text($techno['name']);
+            }
         }
 
 
