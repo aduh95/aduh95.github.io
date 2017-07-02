151cf5e64949b7b658a4c58950a78e861f1195aa
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-07-02 19:45:26 +0200

    Improve i18n

---
+++ front/ts/i18n.ts
@@ -0,0 +1,36 @@
+module aduh95.resume.linkToWebsite {
+
+    if (!location.hash) {
+        let locale =  (<any>window).navigator.userLanguage || window.navigator.language;
+
+        if ("fr" === locale.substr(0, 2)) {
+            locale = "fr-"+(locale.split("-")[1] || "FR");
+        } else {
+            locale = "en-"+(locale.split("-")[1] || "US");
+        }
+
+        location.hash = locale;
+    }
+
+    let changeLanguage = function () {
+        let locale = location.hash.replace(/^#/, "");
+        let progressElem = document.querySelectorAll("time");
+
+        document.documentElement.setAttribute("lang", locale.substr(0, 2));
+
+        for (let elem of <HTMLTimeElement[]><any>progressElem) {
+            let dateTime = elem.dateTime || elem.getAttribute("datetime");
+            elem.innerText = (new Date(dateTime)).toLocaleDateString(
+                locale,
+                {
+                    year: "numeric",
+                    month: "long",
+                }
+            );
+        }
+    };
+
+    window.addEventListener("hashchange", changeLanguage);
+
+    document.addEventListener('DOMContentLoaded', changeLanguage, false);
+}
