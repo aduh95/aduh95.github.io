0e543ae91b85922ef10e2a570da3489e2592d061
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-08-06 23:59:28 +0200

    Remove Storage API

--- front/ts/serviceWorker.ts
+++ front/ts/serviceWorker.ts
@@ -1,37 +1,17 @@
-interface Navigator {
-    storage: StorageManager;
-}
-
-interface StorageManager {
-    estimate: () => StorageEstimate;
-    persist: () => Promise<boolean>;
-    persisted: () => Promise<boolean>;
-}
-
-interface StorageEstimate {
-    quota: number;
-    usage: number;
-}
-
-if ('serviceWorker' in navigator && 'storage' in navigator) {
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.storage.persist().then(function(granted) {
-            if (granted) {
-                // Hurrah, your data is here to stay!
-                navigator.serviceWorker
-                    .register('/sw.js')
-                    .then(registration => {
-                        // Registration was successful
-                        console.log(
-                            'ServiceWorker registration successful with scope: ',
-                            registration.scope
-                        );
-                    })
-                    .catch(err => {
-                        // registration failed :(
-                        console.log('ServiceWorker registration failed: ', err);
-                    });
-            }
-        });
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(registration => {
+                // Registration was successful
+                console.log(
+                    'ServiceWorker registration successful with scope: ',
+                    registration.scope
+                );
+            })
+            .catch(err => {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
     });
 }
