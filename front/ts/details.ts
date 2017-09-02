326a3a8fa15d6660552f7812a0259d52560988c8
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-09-02 19:42:23 +0200

    Add transitions when expending the <detail> elements

--- front/ts/details.ts
+++ front/ts/details.ts
@@ -6,13 +6,30 @@ document.addEventListener(
     'DOMContentLoaded',
     function(this: Document) {
         const SUMMARY_ELEMENT = 'SUMMARY';
+        const detailsSupport = window.hasOwnProperty('HTMLDetailsElement');
         let summaryElem = this.querySelectorAll(SUMMARY_ELEMENT);
         for (let elem of <HTMLElement[]>(<any>summaryElem)) {
-            if (window.hasOwnProperty('HTMLDetailsElement')) {
+            if (detailsSupport) {
                 // Allow the user to close the detail element by clicking on it
+                // And add smooth transition when elements are changing height
                 elem.parentNode.addEventListener(
                     'click',
                     function(this: HTMLDetailsElement, ev: Event) {
+                        if (this.open) {
+                            // Compute the actual height of the element before
+                            // the transition starts
+                            this.style.height = this.offsetHeight + 'px';
+                            window.requestAnimationFrame(() => {
+                                // Set a height on one line to start the transition
+                                this.style.height = '1.5rem';
+                                window.setTimeout(() => {
+                                    // At the end of the transition, removing the
+                                    // height style attribute to let the browser
+                                    // chose the best height
+                                    this.style.height = '';
+                                }, 300);
+                            });
+                        }
                         if (
                             SUMMARY_ELEMENT !==
                                 (<HTMLElement>ev.target).nodeName &&
