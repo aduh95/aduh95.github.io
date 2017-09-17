80bf183ab6219dfeb4c3420216a868f0c5a019c3
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-09-17 13:27:39 +0200

    Update balloon-css

--- front/ts/circularProgress.ts
+++ front/ts/circularProgress.ts
@@ -1,51 +1,48 @@
 namespace aduh95.resume.circularProgress {
-    let insertAfter = function(newNode: Node, referenceNode: Node) {
-        if (referenceNode.nextSibling) {
-            referenceNode.parentNode.insertBefore(
-                newNode,
-                referenceNode.nextSibling
-            );
-        } else {
-            referenceNode.parentNode.appendChild(newNode);
+  let insertAfter = function(newNode: Node, referenceNode: Node) {
+    if (referenceNode.nextSibling) {
+      referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    } else {
+      referenceNode.parentNode.appendChild(newNode);
+    }
+  };
+
+  let getSlice = () => {
+    let slice = document.createElement("div");
+    let bar = document.createElement("div");
+    let fill = document.createElement("div");
+    fill.className = "fill";
+    bar.className = "bar";
+    slice.className = "slice";
+
+    slice.appendChild(bar);
+    slice.appendChild(fill);
+
+    return slice;
+  };
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    function(this: Document) {
+      let progressElem = this.querySelectorAll("meter,progress");
+
+      for (let elem of <HTMLMeterElement[]>(<any>progressElem)) {
+        let newElem = document.createElement("output");
+        if (elem.hasChildNodes()) {
+          for (let child of <HTMLElement[]>(<any>elem.children)) {
+            newElem.appendChild(child.cloneNode(true));
+          }
         }
-    };
+        newElem.appendChild(getSlice());
+        newElem.dataset.title = elem.title;
 
-    let getSlice = () => {
-        let slice = document.createElement("div");
-        let bar = document.createElement("div");
-        let fill = document.createElement("div");
-        fill.className = "fill";
-        bar.className = "bar";
-        slice.className = "slice";
+        // Needed to use CSS hover on iOS (http://www.codehaven.co.uk/fix-css-hover-on-iphone-ipad/)
+        newElem.setAttribute("onclick", "");
 
-        slice.appendChild(bar);
-        slice.appendChild(fill);
-
-        return slice;
-    };
-
-    document.addEventListener(
-        "DOMContentLoaded",
-        function(this: Document) {
-            let progressElem = this.querySelectorAll("meter,progress");
-
-            for (let elem of <HTMLMeterElement[]>(<any>progressElem)) {
-                let newElem = document.createElement("output");
-                if (elem.hasChildNodes()) {
-                    for (let child of <HTMLElement[]>(<any>elem.children)) {
-                        newElem.appendChild(child.cloneNode(true));
-                    }
-                }
-                newElem.appendChild(getSlice());
-                newElem.dataset.title = elem.title;
-
-                // Needed to use CSS hover on iOS (http://www.codehaven.co.uk/fix-css-hover-on-iphone-ipad/)
-                newElem.setAttribute("onclick", "");
-
-                insertAfter(newElem, elem);
-                elem.hidden = true;
-            }
-        },
-        false
-    );
+        insertAfter(newElem, elem);
+        elem.hidden = true;
+      }
+    },
+    false
+  );
 }
