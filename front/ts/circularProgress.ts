0732d02b488e0bb6157b5b58aeb0e5da70e0b10c
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-10-05 16:00:37 +0200

    Compute meter width to homogenize spacings

--- front/ts/circularProgress.ts
+++ front/ts/circularProgress.ts
@@ -21,6 +21,34 @@ namespace aduh95.resume.circularProgress {
     return slice;
   };
 
+  window.addEventListener("load", function() {
+    // Waiting load event to be sure CSS is fully loaded
+    let meterSection = document.querySelectorAll(".meter-section");
+    let canvasContext = document.createElement("canvas").getContext("2d");
+    let maxWidth = 0;
+
+    for (let section of <HTMLElement[]>(<any>meterSection)) {
+      let titles = section.querySelectorAll("h5");
+
+      // Getting the max width of the title elements
+      canvasContext.font = window
+        .getComputedStyle(titles.item(0))
+        .getPropertyValue("font");
+      for (let title of <HTMLElement[]>(<any>titles)) {
+        maxWidth = Math.max(
+          canvasContext.measureText(title.textContent).width,
+          maxWidth
+        );
+      }
+
+      // Setting computed max width as the min width of all the elements
+      for (let title of <HTMLElement[]>(<any>titles)) {
+        // Adding 10px gap as margin
+        title.style.minWidth = maxWidth + 10 + "px";
+      }
+    }
+  });
+
   document.addEventListener(
     "DOMContentLoaded",
     function(this: Document) {
