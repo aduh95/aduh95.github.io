ef58aba282d0640d6fab86766938ae25bef7ce25
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-10-05 09:23:58 +0200

    Use transitionend event instead of setTimeout timer

--- front/ts/details.ts
+++ front/ts/details.ts
@@ -1,59 +1,61 @@
 interface HTMLDetailsElement extends HTMLElement {
-    open: boolean;
+  open: boolean;
 }
 
 document.addEventListener(
-    'DOMContentLoaded',
-    function(this: Document) {
-        const SUMMARY_ELEMENT = 'SUMMARY';
-        const detailsSupport = window.hasOwnProperty('HTMLDetailsElement');
-        let summaryElem = this.querySelectorAll(SUMMARY_ELEMENT);
-        for (let elem of <HTMLElement[]>(<any>summaryElem)) {
-            if (detailsSupport) {
-                // Allow the user to close the detail element by clicking on it
-                // And add smooth transition when elements are changing height
-                elem.parentNode.addEventListener(
-                    'click',
-                    function(this: HTMLDetailsElement, ev: Event) {
-                        if (this.open) {
-                            // Compute the actual height of the element before
-                            // the transition starts
-                            this.style.height = this.offsetHeight + 'px';
-                            window.requestAnimationFrame(() => {
-                                // Waiting for the next frame because of Firefox
-                                window.requestAnimationFrame(() => {
-                                    // Set the height at the last known to start the transition
-                                    this.style.height = this.style.minHeight;
-                                    window.setTimeout(() => {
-                                        // At the end of the transition, removing the
-                                        // height style attribute to let the browser
-                                        // chose the best height
-                                        this.style.height = '';
-                                        this.style.minHeight = '';
-                                    }, 300);
-                                });
-                            });
-                        } else {
-                            // Saving the current height to allow sweet transition
-                            this.style.minHeight =
-                                (<HTMLElement>this.firstElementChild)
-                                    .offsetHeight + 'px';
-                        }
-                        if (
-                            SUMMARY_ELEMENT !==
-                                (<HTMLElement>ev.target).nodeName &&
-                            this.open
-                        ) {
-                            this.open = false;
-                        }
-                    },
-                    true
-                );
+  "DOMContentLoaded",
+  function(this: Document) {
+    const SUMMARY_ELEMENT = "SUMMARY";
+    const detailsSupport = window.hasOwnProperty("HTMLDetailsElement");
+    let summaryElem = this.querySelectorAll(SUMMARY_ELEMENT);
+    for (let elem of <HTMLElement[]>(<any>summaryElem)) {
+      if (detailsSupport) {
+        // Allow the user to close the detail element by clicking on it
+        // And add smooth transition when elements are changing height
+        elem.parentNode.addEventListener(
+          "click",
+          function(this: HTMLDetailsElement, ev: Event) {
+            if (this.open) {
+              // Compute the actual height of the element before
+              // the transition starts
+              this.style.height = this.offsetHeight + "px";
+              window.requestAnimationFrame(() => {
+                // Waiting for the next frame because of Firefox
+                window.requestAnimationFrame(() => {
+                  // Set the height at the last known to start the transition
+                  this.style.height = this.style.minHeight;
+                });
+              });
+              ev.target.addEventListener(
+                "transitionend",
+                (event: TransitionEvent) => {
+                  // At the end of the transition, removing the
+                  // height style attribute to let the browser
+                  // chose the best height
+                  this.style.height = "";
+                  this.style.minHeight = "";
+                },
+                { once: true, passive: true }
+              );
             } else {
-                // For browsers that do not support <details>, let's hide the summaries
-                elem.hidden = true;
+              // Saving the current height to allow sweet transition
+              this.style.minHeight =
+                (<HTMLElement>this.firstElementChild).offsetHeight + "px";
             }
-        }
-    },
-    false
+            if (
+              SUMMARY_ELEMENT !== (<HTMLElement>ev.target).nodeName &&
+              this.open
+            ) {
+              this.open = false;
+            }
+          },
+          true
+        );
+      } else {
+        // For browsers that do not support <details>, let's hide the summaries
+        elem.hidden = true;
+      }
+    }
+  },
+  false
 );
