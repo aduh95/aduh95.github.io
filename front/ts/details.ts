3ea37b4522090869834b4ebbaa2e0d7d29296263
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-05-07 01:21:11 +0200

    Update experience animations with JS

--- front/ts/details.ts
+++ front/ts/details.ts
@@ -6,45 +6,115 @@ document.addEventListener(
   "DOMContentLoaded",
   function(this: Document) {
     const SUMMARY_ELEMENT = "SUMMARY";
+    const MOVABLE_ELEMENT_CLASS = "movable-element";
+    const MOVING_ELEMENTS_CLASS = "moving-elements";
     const detailsSupport = window.hasOwnProperty("HTMLDetailsElement");
-    let summaryElem = this.querySelectorAll(SUMMARY_ELEMENT);
-    for (let elem of <HTMLElement[]>(<any>summaryElem)) {
+    const summaryElem = this.querySelectorAll(SUMMARY_ELEMENT);
+
+    const animateElementsBelow = (
+      parentElement: HTMLElement,
+      height: number,
+      callback: Function
+    ) => {
+      // The goal of this fuunction is to make the animation smoother using JS than
+      // the one using only CSS. However, if the user disables JS, the animation still works.
+      const articleIndex = Array.prototype.indexOf.call(
+        document.querySelectorAll("main>.experience>*"),
+        parentElement
+      );
+      const movableElements = document.querySelectorAll(
+        [
+          "main>.experience~section",
+          `main>.experience>article:nth-child(n + ${articleIndex + 2})`,
+          `main>.experience>article:nth-child(${articleIndex + 1})>details+*`,
+        ].join(",")
+      );
+
+      document.body.style.setProperty("--movable-height", height + "px");
+      document.body.classList.add(MOVING_ELEMENTS_CLASS);
+      parentElement.classList.add(MOVABLE_ELEMENT_CLASS + "-after");
+      for (const movableElement of <HTMLElement[]>(<any>movableElements)) {
+        movableElement.classList.add(MOVABLE_ELEMENT_CLASS);
+      }
+
+      // When the animation has ended, cleaning up
+      movableElements.item(0).addEventListener(
+        "transitionend",
+        () => {
+          callback();
+          parentElement.classList.remove(MOVABLE_ELEMENT_CLASS + "-after");
+          for (const movableElement of <HTMLElement[]>(<any>movableElements)) {
+            movableElement.classList.remove(MOVABLE_ELEMENT_CLASS);
+          }
+          document.body.classList.remove(MOVING_ELEMENTS_CLASS);
+        },
+        { once: true, passive: true }
+      );
+    };
+
+    for (const elem of <HTMLElement[]>(<any>summaryElem)) {
       if (detailsSupport) {
         // Allow the user to close the detail element by clicking on it
         // And add smooth transition when elements are changing height
         elem.parentNode.addEventListener(
           "click",
           function(this: HTMLDetailsElement, ev: Event) {
-            let shouldClose = this.open && window.getSelection().isCollapsed;
+            const shouldClose = this.open && window.getSelection().isCollapsed;
             if (shouldClose) {
               // Compute the actual height of the element before
               // the transition starts
-              this.style.height = this.offsetHeight + "px";
+              const currentHeight = this.offsetHeight;
+              this.style.height = currentHeight + "px";
               window.requestAnimationFrame(() => {
-                // Waiting for the next frame because of Firefox
-                window.requestAnimationFrame(() => {
-                  // Set the height at the last known to start the transition
-                  this.style.height = this.style.minHeight;
-                });
+                const minHeight = parseInt(this.style.minHeight.slice(0, -2));
+
+                animateElementsBelow(
+                  this.parentElement,
+                  minHeight - currentHeight,
+                  () => {
+                    // Set the height at the last known to start the transition
+                    this.style.height = "";
+                    this.style.minHeight = "";
+                  }
+                );
               });
-              ev.currentTarget.addEventListener(
-                "transitionend",
-                (event: TransitionEvent) => {
-                  // At the end of the transition, removing the
-                  // height style attribute to let the browser
-                  // chose the best height
-                  this.style.height = "";
-                  this.style.minHeight = "";
-                },
-                { once: true, passive: true }
-              );
             } else {
+              const summaryHeight = (<HTMLElement>this.firstElementChild)
+                .offsetHeight;
+
               // Unsetting the height in case the transition did not end
               this.style.height = "";
 
               // Saving the current height to allow sweet transition
-              this.style.minHeight =
-                (<HTMLElement>this.firstElementChild).offsetHeight + "px";
+              this.style.minHeight = summaryHeight + "px";
+
+              // Selecting the paragraph that will appear
+              const paragraph = <HTMLElement>this.querySelector(
+                `p[lang='${document.documentElement.getAttribute("lang")}']`
+              );
+
+              // Hiding the paragraph to compute its height
+              paragraph.style.opacity = "0";
+              paragraph.style.position = "absolute";
+              paragraph.style.animation = "unset";
+
+              // Waiting for the paragraph to appear next frame
+              requestAnimationFrame(() => {
+                const { height } = paragraph.getBoundingClientRect();
+
+                // Triggers CSS animation
+                animateElementsBelow(
+                  this.parentElement,
+                  height - summaryHeight,
+                  () => {
+                    paragraph.style.removeProperty("position");
+                  }
+                );
+
+                // The paragraph can re-appear and make its transition now we know its height
+                paragraph.style.removeProperty("opacity");
+                paragraph.style.removeProperty("animation");
+              });
             }
             if (
               SUMMARY_ELEMENT !== (<HTMLElement>ev.target).nodeName &&
