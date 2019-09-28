70195f21f06169d87ed5261f54ed681557b34c6e
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-09-28 11:38:33 -0400

    Fix service worker script

--- src/sw.ts
+++ src/sw.ts
@@ -1,38 +1,34 @@
-/// <reference path="../node_modules/typescript/lib/lib.webworker.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es5.d.ts"/>
-declare var Promise: any;
-
-'use strict';
 var serviceWorker: ServiceWorkerGlobalScope = <any>self;
 
-const version = '0.0.3';
-const cacheName = 'sw-aduh95';
-const cache = cacheName + '-' + version;
+const version = "0.0.4";
+const cacheName = "sw-aduh95";
+const cache = cacheName + "-" + version;
 
-serviceWorker.addEventListener('install', function(event: ExtendableEvent) {
-    event.waitUntil(serviceWorker.skipWaiting());
+serviceWorker.addEventListener("install", function(event: ExtendableEvent) {
+  event.waitUntil(serviceWorker.skipWaiting());
 });
 
 //Add event listener for fetch
-serviceWorker.addEventListener('fetch', function(event: FetchEvent) {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+serviceWorker.addEventListener("fetch", function(event: FetchEvent) {
+  event.respondWith(
+    fetch(event.request).catch(
+      () => caches.match(event.request) as Promise<Response>
+    )
+  );
 });
 
-serviceWorker.addEventListener('activate', function(event: ExtendableEvent) {
-    event.waitUntil(
-        caches
-            .keys() //it will return all the keys in the cache as an array
-            .then((keyList: Array<string>) =>
-                //run everything in parallel using Promise.all()
-                Promise.all(
-                    keyList.map(
-                        key =>
-                            key !== cacheName ? caches.delete(key) : undefined
-                    )
-                )
-            )
-            .then(() => serviceWorker.clients.claim())
-    );
+serviceWorker.addEventListener("activate", function(event: ExtendableEvent) {
+  event.waitUntil(
+    caches
+      .keys() //it will return all the keys in the cache as an array
+      .then((keyList: Array<string>) =>
+        //run everything in parallel using Promise.all()
+        Promise.all(
+          keyList.map(key =>
+            key !== cacheName ? caches.delete(key) : undefined
+          )
+        )
+      )
+      .then(() => serviceWorker.clients.claim())
+  );
 });
