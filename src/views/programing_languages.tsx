bc7f472415fd9dbb48eba8df961b857a775d4f70
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2019-12-03 17:21:47 +0100

    sort Programming Languages list

--- src/views/programing_languages.tsx
+++ src/views/programing_languages.tsx
@@ -9,15 +9,19 @@ export default function ProgramingLanguages() {
     <section className="programming_languages meter-section">
       <h3 lang="en">Programming Languages</h3>
       <h3 lang="fr">Programmation informatique</h3>
-      {languages.map(section => (
-        <article>
-          <h5>{section.name}</h5>
-          <meter
-            value={section.level}
-            title={Number(section.level) * 100 + "%" || ""}
-          />
-        </article>
-      ))}
+      {languages
+        .sort((el1, el2) =>
+          el1.level > el2.level ? -1 : el1.level < el2.level ? 1 : 0
+        )
+        .map(section => (
+          <article>
+            <h5>{section.name}</h5>
+            <meter
+              value={section.level}
+              title={Number(section.level) * 100 + "%" || ""}
+            />
+          </article>
+        ))}
     </section>
   );
 }
