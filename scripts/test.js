d6c781de53457bf15615ed1b0b468b69c08f1bab
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-01-15 17:12:45 +0100

    Add tests

---
+++ scripts/test.js
@@ -0,0 +1,19 @@
+import { promises as fs } from "fs";
+import path from "path";
+import assert from "assert";
+import parse from "pdf-parse";
+
+const dir = path.resolve(process.argv[2]);
+
+fs.readdir(dir)
+  .then(files =>
+    Promise.all(
+      files.map(file => fs.readFile(path.join(dir, file)).then(parse))
+    )
+  )
+  .then(parseResults =>
+    assert(
+      parseResults.every(({ numpages }) => numpages === 1),
+      "All PDF documents should contain only one page."
+    )
+  );
