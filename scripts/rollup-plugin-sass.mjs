7ba4f55d398b7d27015cb7eb7b3b9b3a00dec2b6
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-05-13 11:41:28 +0200

    Optimize rollup-plugin-sass

--- scripts/rollup-plugin-sass.mjs
+++ scripts/rollup-plugin-sass.mjs
@@ -1,6 +1,8 @@
 import sass from "sass";
 import fiber from "fibers";
 
+const PLUGIN_HELPER = "sass-plugin:createStyleElement";
+
 function createStyleElement(css, id) {
   const style = document.createElement("style");
   style.textContent = css;
@@ -13,29 +15,33 @@ export default function plugin() {
   return {
     name: "sass",
     resolveId(source) {
-      // this signals that rollup should not ask other plugins or check the file system to find this id
-      return source.endsWith(".scss") ? source : null;
+      // This signals that rollup should not ask other plugins or check the file
+      // system to find this id.
+      return source.endsWith(".scss") || source === PLUGIN_HELPER
+        ? source
+        : null;
     },
     load(id) {
-      if (!id.endsWith(".scss")) {
-        return null;
+      if (id === PLUGIN_HELPER) {
+        return `export default ${createStyleElement};`;
+      } else if (id.endsWith(".scss")) {
+        return new Promise((resolve, reject) =>
+          sass.render(
+            {
+              file: id,
+              fiber,
+              sourceMap: "true",
+              sourceMapEmbed: true,
+            },
+            (err, result) => (err ? reject(err) : resolve(result))
+          )
+        ).then(
+          ({ css }) =>
+            `import helper from "${PLUGIN_HELPER}";export default helper(\`${css
+              .toString()
+              .replace("`", "\\`")}\`,"${id.replace('"', '\\"')}")`
+        );
       }
-      return new Promise((resolve, reject) =>
-        sass.render(
-          {
-            file: id,
-            fiber,
-            sourceMap: "true",
-            sourceMapEmbed: true,
-          },
-          (err, result) => (err ? reject(err) : resolve(result))
-        )
-      ).then(
-        ({ css }) =>
-          `export default (${createStyleElement})(\`${css
-            .toString()
-            .replace("`", "\\`")}\`,"${id.replace('"', '\\"')}")`
-      );
     },
   };
 }
