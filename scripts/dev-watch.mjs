36527e85098e492675ddfcebfb3e8a4bb3a84520
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2020-05-15 14:47:45 +0200

    watch: avoid sending rebuild signal twice to improve perf

--- scripts/dev-watch.mjs
+++ scripts/dev-watch.mjs
@@ -6,10 +6,17 @@ import { sendRebuildSignal } from "./dev-build-js-from-worker.mjs";
 import { refreshBrowser } from "./dev-server.mjs";
 import { startServer } from "./dev-server.mjs";
 
+let antiRebound;
 const watcher = (event, fileName) => {
   console.log(event, fileName);
-  sendRebuildSignal(fileName);
-  refreshBrowser();
+  if (antiRebound !== fileName) {
+    sendRebuildSignal(fileName);
+    antiRebound = fileName;
+    setTimeout(() => {
+      if (antiRebound === fileName) antiRebound = null;
+    }, 5000);
+    refreshBrowser();
+  }
 };
 
 const watchFile = (path) => watch(path, watcher);
